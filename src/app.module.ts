877b8d0d1885269fc5fa5bc20f01d9ff1bd780d0
Manish Chhetri <65161301+chhetri123@users.noreply.github.com>
2023-11-08 15:00:56 +0000

    Database and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
