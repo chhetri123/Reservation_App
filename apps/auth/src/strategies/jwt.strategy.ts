5525439ce368ef158f9c8ea74185c4b0b084320d
Mandip Chhetri <108076760+Mandip47@users.noreply.github.com>
2023-11-17 16:48:29 +0000

    Implementing jwt strategy and adding jwt auth guard

---
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable, Logger } from "@nestjs/common";
+import {  ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { UsersService } from "../users/users.service";
+import { Request } from "express";
+import { TokenPayload } from "../interfaces/token-payload.interface";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private readonly config: ConfigService,
+        private readonly userService: UsersService
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromExtractors([(request:Request)=>request?.cookies?.Authentication]),
+            secretOrKey: config.get('JWT_SECRET'),
+        });
+    }
+
+    async validate(payload: TokenPayload) {
+        const user = await this.userService.getUser({_id: payload.userId});
+                return user;
+      
+    }
+}
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,12 +1,15 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
 import { UserRepository } from './users.repository';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcryptjs';
+import { GetUserDto } from './dto/get-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UserRepository) {}
   async create(createUserDto: CreateUserDto) {
+
+    await this.validateCreateUserDto(createUserDto)
     return this.usersRepository.create({
       ...createUserDto,
       password: await bcrypt.hash(createUserDto.password, 10),
@@ -21,5 +24,20 @@ export class UsersService {
       throw new UnauthorizedException(`Credentials are not valid`);
     }
     return user;
+  }
+  async getUser(getUserDto: GetUserDto) { 
+    return this.usersRepository.findOne({
+      _id:getUserDto._id
+    });
+  }
+
+  async validateCreateUserDto(createUserDto) {
+    try {
+      await this.usersRepository.findOne({email:createUserDto.email})
+    } catch (err) {
+      return
+    }
+    throw new UnprocessableEntityException("Email Already Exist")
+    
   }
 }
